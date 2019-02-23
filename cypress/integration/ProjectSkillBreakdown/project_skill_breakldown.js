ae576d59cc022f5acc2b0003c523cb4c3a1649e3
aaron <aaron@mindvalley.com>
2019-02-23 13:13:57 +0800

    step definition for arbitrary clicks
    Signed-off-by: aaron <aaron@mindvalley.com>

---
+++ cypress/integration/ProjectSkillBreakdown/project_skill_breakldown.js
@@ -0,0 +1,19 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import {Given, Then, When} from 'cypress-cucumber-preprocessor/steps';
+
+const url = 'localhost:8080';
+Given('a user on the home page', () => {
+  cy.visit(url);
+});
+
+When('they click {string}', (cls) => {
+  cy.get(cls).click()
+});
+
+Then(`the {string} will have one child element`, (cls) => {
+  cy.get(cls).should('have.length', 1);
+});
+
+Then(`the {string} will be visible`, (cls) => {
+  cy.get(cls).should('be.visible');
+});
