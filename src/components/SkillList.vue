93e03db6a78f7eabb1542775e1373a2ec683c815
aaron <aaron@mindvalley.com>
2019-03-01 00:08:11 +0800

    adds skeleton SkillList component
    closes issue #6
    
    Signed-off-by: aaron <aaron@mindvalley.com>

---
+++ src/components/SkillList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ol class="skill_list"></ol>
+</template>
+
+<script>
+  // @todo #4:30m/DEV implement Add Skill on click adds a "project_item" to ".skill_list
+
+  // @todo #4:30m/DEV create Skill component
+
+  // @todo #4:30m/DEV SkillList should aggregate the skill "weights" and present them
+  //  I am imagining all weights should total to no more than 100%
+
+  export default {
+    name: 'SkillList',
+    props: {},
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
