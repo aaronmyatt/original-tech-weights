2750495224c936ad6f4be605fa50a130557df41c
aaron <aaron@mindvalley.com>
2019-03-03 01:10:14 +0800

    can populate the skills list
    closes issue #23
    
    Signed-off-by: aaron <aaron@mindvalley.com>

--- src/components/SkillList.vue
+++ src/components/SkillList.vue
@@ -1,11 +1,20 @@
 <template>
-  <ol class="skill-list">
-    <li>Skill #1</li>
-  </ol>
+  <div>
+    <label for="skill-name-input">Skill</label>
+    <input id="skill-name-input" type="text" v-model="skillName">
+    <label for="skill-weight-input">Skill</label>
+    <input id="skill-weight-input" type="text" v-model="skillWeight">
+    <button @click="addSkill()" type="button"> Add Skill </button>
+    <ol class="skill-list" v-for="skill in skills">
+      <li class="skill-item">{{ skill.name }} | {{ skill.weight }}</li>
+    </ol>
+  </div>
 </template>
 
 <script>
-// @todo #4:30m/DEV implement Add Skill on click adds a "skill-item" to ".skill-list
+// @todo #23:30m/DEV add validation for weight field (integer only)
+
+// @todo #23:30m/DEV the sum of all skill weights should not exceed 100
 
 // @todo #4:30m/DEV create Skill component
 
@@ -18,6 +27,23 @@
 export default {
   name: 'SkillList',
   props: {},
+  data: () => {
+    return {
+      skills: [],
+      skillName: "",
+      skillWeight: 0,
+    }
+  },
+  methods: {
+    addSkill() {
+      this.skills.push({
+        name: this.skillName,
+        weight: this.skillWeight,
+      });
+      // prevents page refresh
+      return false;
+    },
+  },
 };
 </script>
 
